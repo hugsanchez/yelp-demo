00b45271a45631a86f9cd82686fbe6515fdf3257
hugsanchez <73506780+hugsanchez@users.noreply.github.com>
2023-08-04 01:25:19 -0400

    added route to connect to index.html

--- server/server.js
+++ server/server.js
@@ -2,11 +2,19 @@ require("dotenv").config();
 
 const express  = require("express");
 const app = express();
+const path = require("path");
+
 
 app.use(express.json());
 
 const restauRouter = require('./api/restauRouter');
 app.use('/api/v1/restaurants', restauRouter);
+
+app.use(express.static(path.join(__dirname, '../client/public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
 
 const port = process.env.PORT || 3001;
 
